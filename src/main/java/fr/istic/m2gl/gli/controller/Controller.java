9bc0fe4af27fe586805b6fb34e39c8bc4141e753
amandine <didinem2@gmail.com>
2014-10-24 10:01:24 +0200

    update

--- src/main/java/fr/istic/m2gl/gli/controller/Controller.java
+++ src/main/java/fr/istic/m2gl/gli/controller/Controller.java
@@ -3,20 +3,23 @@ package fr.istic.m2gl.gli.controller;
 import javafx.event.EventHandler;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import fr.istic.m2gl.gli.model.*;
 import fr.istic.m2gl.gli.view.BoardView;
 
 public class Controller{
 
 	public Controller(BoardView boardView){
-		
-		
-		
+		Board BoardImpl = new BoardImpl(8);
+		BoardImpl.commit();
+
+
+
 		boardView.setOnKeyPressed(new EventHandler<KeyEvent>(){
 			public void handle(KeyEvent ke){
 				System.out.println("Key pressed");
 			}
 		});
-		
+
 		boardView.setOnMousePressed(new EventHandler<MouseEvent>(){
 			public void handle(MouseEvent ke){
 				System.out.println("Mouse pressed");
